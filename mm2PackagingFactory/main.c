71bec3e9e3bbf0cd24f59fe86f28b25c887e1b0d
jeremy brookfield <jeremybrookfield@jbmbph.local>
2014-03-22 15:31:48 +0100

    Initial Commit

---
+++ mm2PackagingFactory/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  mm2PackagingFactory
+//
+//  Created by jeremy brookfield on 22/03/14.
+//  Copyright (c) 2014 jeremy brookfield. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
