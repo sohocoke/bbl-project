5b649b0f98b9d89d207fe276151639a1dfbdc6a6
jeremy brookfield <jeremybrookfield@jbmbph.local>
2014-03-31 01:42:42 +0200

    MDX creation is working

---
+++ mm2PackagingFactory/main.m
@@ -0,0 +1,73 @@
+//
+//  main.c
+//  mm2PackagingFactory
+//
+//  Created by jeremy brookfield on 22/03/14.
+//
+
+
+#import "MDXHandler.h"
+#import "XMLDatasetHandler.h"
+#import "ArchiveHandler.h"
+#import "UploadToAppC.h"
+#import "PListHandler.h"
+#import "Configuration.h"
+
+int main(int argc, const char * argv[])
+{
+    NSLog(@"MM2 Packaging Factory is starting");
+    
+    if (argc != 4)
+    {
+        NSLog(@"Invalid number of args passed: Usage is mm2PackaginFactory pathToAppCList.xml pathToMM2AppList.xml pathToControlList.xml");
+    }
+    
+    Configuration* configuration = [Configuration getInstance]; //Configuration is a singleton
+
+    [configuration setPathToAppcList:[NSString stringWithUTF8String:argv[1]]];
+    [configuration setPathToMM2AppList:[NSString stringWithUTF8String:argv[2]]];
+    [configuration setPathToControlList:[NSString stringWithUTF8String:argv[3]]];
+    
+    
+    //create MDX
+    MDXHandler * mdxHandler = [[MDXHandler alloc] init];
+    MDXApp* mdxApp = [[MDXApp alloc] initWithName:@"worxmail" displayName:@"WorxMail HKG"];
+    if ([mdxHandler createInternal:mdxApp]) {
+        
+    }
+    
+
+
+    //open, modify and replace the info.plist
+//    PListHandler * plistHandler = [[PListHandler alloc] init];
+//    __unused NSData * mData = [plistHandler readAndUpdate];
+    
+    //Connect to AppController
+//    UploadToAppC * uploadToAppC = [[UploadToAppC alloc] init];
+//    __unused NSData * mData = [uploadToAppC open];
+
+    //open and then compress an archive
+//    ArchiveHandler * archiveHandler = [[ArchiveHandler alloc] init];
+//    __unused NSData * openArchive = [archiveHandler open];
+//    __unused NSData * zipped = [archiveHandler compress];
+    
+    //XML Handling
+//    XMLDatasetHandler *xmlDatasetHandler = [[XMLDatasetHandler alloc] init];
+//    __unused NSData *  xmlStructure = [xmlDatasetHandler dictionaryFromXML];
+    //read, update and write to a new location an XML Document
+//    __unused NSData *  xmlStructure = [xmlDatasetHandler readAndUpdate];
+
+    
+    //Issue and read the response from an "ls -a" command
+    //ExecuteProgram *executeProgram = [[ExecuteProgram alloc] init];
+    //__unused NSData *  commandResult = [executeProgram invoke];
+    
+ 
+    
+    NSLog(@"MM2 Packaging Factory finished");
+    
+    
+    
+    return 0;
+}
+
