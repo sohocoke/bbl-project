5e885a77c61979c28639a7ccb1c8d4cea3476ade
Andy Park <sang.park@credit-suisse.com>
2014-09-05 15:41:27 +0200

    /apps API endpoint.

--- server/app.rb
+++ server/app.rb
@@ -1,10 +1,29 @@
 require 'sinatra'
+require 'json'
 
-get '/hi' do
-	"Hello"
+require 'sinatra/base'
+
+class Server < Sinatra::Base
+
+  # a simple GET API for list data.
+  get '/apps' do
+    # CORS permission for angular dev-phase resources
+    # FIXME reduce scope to dev only.
+    response['Access-Control-Allow-Origin'] = "*"
+    [
+      {
+        id: "ch-etit"
+      },
+      {
+        id: "ch-prod"
+      }
+    ].to_json
+  end
+
+
+  # TODO set up a POST endpoint for command exec.
+
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
-
-# TODO serve up a simple GET API for list data.
-
-
-# TODO set up a POST endpoint for command exec.
