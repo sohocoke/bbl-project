095d6649f1c34190f4ce89e5d04c087e6b6e9c0f
Andy Park <andy@bigbearlabs.com>
2014-09-16 11:39:45 +0200

    fix default params to #apps

--- lib/configs.rb
+++ lib/configs.rb
@@ -46,7 +46,7 @@ def targets(pattern)
 end
 
 
-def apps(pattern, opts = { variants: true })
+def apps(pattern = /.*/, opts = { variants: true })
     Dir.glob("#{Base_dir}/apps/**/config.yaml")
         .map {|e| File.read e}
         .map {|e| YAML.load(e)}
@@ -222,7 +222,12 @@ def policy_applied policy_xml, policy_delta
     doc = XML::Document.string policy_xml    
 
     policy_delta.each do |k, v|
-        predicate_val = /.+\[.+='(.*)'\]/.match(k)[1]
+        predicate_val = 
+            if (matches = /.+\[.+='(.*)'\]/.match(k))
+                matches[1]
+            else
+                raise "no predicate in #{k}"
+            end
         
         p "applying value '#{v}' to policy '#{predicate_val}'"
         
